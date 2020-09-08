1436bd06d1d0415e07f0e50679acbd63554bf5db
AmrSallam10 <amrsallam1000@gmail.com>
2020-09-08 11:56:10 +0200

    main

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include"max.h"
+#include"knapsack.h"
+
+int main()
+{
+	int val[]={60,100,120};
+	int wt[]={10,20,30};
+	int W =50;
+	int n =sizeof (val) / sizeof (val[0]);
+	cout<<knapsack(W,wt,val,n);
+	return 0;
+}
